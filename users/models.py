5a5d5f4390737f9edfaa65ca3506b3a7c22ebbf9
Lucas Souza <lucas.soaresouza@gmail.com>
2019-12-24 01:43:31 -0300

    Adicionando campo de permissoes a tabela de CustomUser

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
-class CustomUser(AbstractUser):
-    name = models.CharField(max_length=255, blank=True, default="")
-    notification_token = models.CharField(max_length=255, default="")
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(help_text= "Nome do usuario", max_length=255, blank=True, default="")
+    username = models.CharField(help_text='Identificador do usuario', max_length=100, blank=False, unique=True)
+    permissions = ArrayField(models.CharField(max_length=255, blank=True), default=list)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name','permissions']
 
     def __str__(self):
         return self.username
